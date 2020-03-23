e01734e2bee79d48c52dae07570dacceba5481eb
Piero <grilli.piero@circeco.org>
2020-03-23 17:32:08 +0000

    routing challenge

--- run.py
+++ run.py
@@ -4,11 +4,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT", "5000")),
+            port=int(os.environ.get("PORT", "8000")),
             debug=True)
